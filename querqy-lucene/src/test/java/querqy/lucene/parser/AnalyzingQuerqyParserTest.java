cca3b9bbf5f16069f5dfb1d6d15295b727e07fbe
Markus Heiden <markus.heiden@s24.com>
2014-08-12 12:19:40 +0200

    Reworked AnalyzingQuerqyParserTest

--- querqy-lucene/src/test/java/querqy/lucene/parser/AnalyzingQuerqyParserTest.java
+++ querqy-lucene/src/test/java/querqy/lucene/parser/AnalyzingQuerqyParserTest.java
@@ -5,13 +5,17 @@ import static querqy.QuerqyMatchers.dmq;
 import static querqy.QuerqyMatchers.term;
 
 import java.io.Reader;
+import java.util.Collections;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.MockTokenizer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.TypeTokenFilter;
 import org.apache.lucene.analysis.synonym.SynonymFilter;
 import org.apache.lucene.analysis.synonym.SynonymMap;
 import org.apache.lucene.util.CharsRef;
 import org.apache.lucene.util.LuceneTestCase;
+import org.apache.lucene.util.Version;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -30,51 +34,59 @@ public class AnalyzingQuerqyParserTest extends LuceneTestCase {
 
    /**
     * Synonym analyzer.
-    * Single synonym "synonym" for "test". 
+    * Just adds the synonyms "synonym1" and "synonym2" for "test". 
     */
-   private Analyzer optSynonymAnalyzer;
+   private Analyzer synonymAnalyzer;
 
    @Before
    public void createAnalyzers() throws Exception {
       queryAnalyzer = new Analyzer() {
          @Override
          protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+            // White space tokenizer, to lower case tokenizer.
             return new TokenStreamComponents(new MockTokenizer(reader));
          }
       };
 
       SynonymMap.Builder builder = new SynonymMap.Builder(true);
-      builder.add(new CharsRef("test"), new CharsRef("synonym"), false);
+      builder.add(new CharsRef("test"), new CharsRef("synonym1"), false);
+      builder.add(new CharsRef("test"), new CharsRef("synonym2"), false);
       final SynonymMap synonyms = builder.build();
 
-      optSynonymAnalyzer = new Analyzer() {
+      synonymAnalyzer = new Analyzer() {
          @Override
          protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+            // White space tokenizer, to lower case tokenizer.
             MockTokenizer tokenizer = new MockTokenizer(reader);
-            return new TokenStreamComponents(tokenizer, new SynonymFilter(tokenizer, synonyms, true));
+            // Filter for adding synonyms
+            TokenStream result = new SynonymFilter(tokenizer, synonyms, true);
+            // Filter all non-synonyms, because the synonym filter outputs the original token too.
+            result = new TypeTokenFilter(Version.LUCENE_4_9, result, Collections.singleton(SynonymFilter.TYPE_SYNONYM), true);
+            return new TokenStreamComponents(tokenizer, result);
          }
       };
    }
 
    /**
-    * Test for {@link AnalyzingQuerqyParser#parse(String)}.
+    * Test for {@link AnalyzingQuerqyParser#parse(String)} without synonyms.
     */
    @Test
    public void parse_withoutSynonyms() {
       QuerqyParser parser = new AnalyzingQuerqyParser(queryAnalyzer, null);
-      Query query = parser.parse("test");
+      Query query = parser.parse("test dummy");
       
-      assertThat(query, bq(dmq(term("test"))));
+      assertThat(query, bq(dmq(term("test")), dmq(term("dummy"))));
    }
 
    /**
-    * Test for {@link AnalyzingQuerqyParser#parse(String)}.
+    * Test for {@link AnalyzingQuerqyParser#parse(String)} with synonyms.
     */
    @Test
    public void parse_withSynonyms() {
-      QuerqyParser parser = new AnalyzingQuerqyParser(queryAnalyzer, optSynonymAnalyzer);
-      Query query = parser.parse("test");
+      QuerqyParser parser = new AnalyzingQuerqyParser(queryAnalyzer, synonymAnalyzer);
+      Query query = parser.parse("test dummy");
 
-      assertThat(query, bq(dmq(term("test"), term("synonym"))));
+      // "test" with its synonyms, "dummy" without synonyms, because none are defined for it.
+      assertThat(query, bq(dmq(term("test"), term("synonym1"), term("synonym2")), dmq(term("dummy"))));
    }
 }
