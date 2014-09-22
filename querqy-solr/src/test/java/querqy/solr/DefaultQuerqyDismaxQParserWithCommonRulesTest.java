6e750eab8077a14074fb6b6883764211733acfee
René Kriegler <rk@rene-kriegler.de>
2014-09-23 01:31:34 +0200

    Adding some Solr CR tests

--- querqy-solr/src/test/java/querqy/solr/DefaultQuerqyDismaxQParserWithCommonRulesTest.java
+++ querqy-solr/src/test/java/querqy/solr/DefaultQuerqyDismaxQParserWithCommonRulesTest.java
@@ -56,5 +56,47 @@ public class DefaultQuerqyDismaxQParserWithCommonRulesTest extends SolrTestCaseJ
         
         
     }
+    
+    
+    @Test
+    public void testDeleteIsAppliedInContext() throws Exception {
+        String q = "t1 t2";
+
+        SolrQueryRequest req = req("q", q,
+              DisMaxParams.QF, "f1 f2 f3",
+              DisMaxParams.MM, "1",
+              QueryParsing.OP, "OR",
+              "defType", "querqy",
+              "debugQuery", "on"
+              );
+
+        assertQ("Delete in context fails",
+              req,
+              "//str[@name='parsedquery'][not(contains(.,'t2'))]"
+        );
+
+        req.close();
+    }
+    
+    @Test
+    public void testThatRuleMatchingIsCaseInsensitive() throws Exception {
+        String q = "T1 T2";
+
+        SolrQueryRequest req = req("q", q,
+              DisMaxParams.QF, "f1 f2 f3",
+              DisMaxParams.MM, "1",
+              QueryParsing.OP, "OR",
+              "defType", "querqy",
+              "debugQuery", "on"
+              );
+
+        assertQ("Rule matching seems to be case sensitive",
+              req,
+              "//str[@name='parsedquery'][not(contains(.,'t2'))]",
+              "//str[@name='parsedquery'][not(contains(.,'T2'))]"
+        );
+
+        req.close();
+    }
 
 }
