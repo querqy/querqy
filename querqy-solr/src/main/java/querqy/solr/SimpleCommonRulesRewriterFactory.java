6d7bf69689c421661763bac0ce09d3eec0a0d9f1
René Kriegler <rk@rene-kriegler.de>
2014-10-20 19:03:28 +0100

    Setting encoding to UTF-8 when reading Common Rules

--- querqy-solr/src/main/java/querqy/solr/SimpleCommonRulesRewriterFactory.java
+++ querqy-solr/src/main/java/querqy/solr/SimpleCommonRulesRewriterFactory.java
@@ -4,6 +4,7 @@
 package querqy.solr;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.lucene.analysis.util.ResourceLoader;
 import org.apache.solr.common.util.NamedList;
@@ -12,7 +13,7 @@ import querqy.parser.QuerqyParserFactory;
 import querqy.rewrite.RewriterFactory;
 
 /**
- * @author rene
+ * @author René Kriegler, @renekrie
  *
  */
 public class SimpleCommonRulesRewriterFactory implements RewriterFactoryAdapter {
@@ -44,8 +45,9 @@ public class SimpleCommonRulesRewriterFactory implements RewriterFactoryAdapter 
             querqyParser = resourceLoader.newInstance(rulesQuerqyParser, QuerqyParserFactory.class);
          }
       }
+      
       return new querqy.rewrite.commonrules.SimpleCommonRulesRewriterFactory(
-            resourceLoader.openResource(rulesResourceName), querqyParser, ignoreCase != null && ignoreCase);
+            new InputStreamReader(resourceLoader.openResource(rulesResourceName), "UTF-8"), querqyParser, ignoreCase != null && ignoreCase);
    }
 
 }
