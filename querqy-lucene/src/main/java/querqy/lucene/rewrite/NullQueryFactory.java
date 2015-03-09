174a0d867fd7698817b940b2c71646f8155d7bac
René Kriegler <rk@rene-kriegler.de>
2015-03-09 11:49:03 +0000

    Simplifying NullQuery

--- querqy-lucene/src/main/java/querqy/lucene/rewrite/NullQueryFactory.java
+++ querqy-lucene/src/main/java/querqy/lucene/rewrite/NullQueryFactory.java
@@ -5,9 +5,7 @@ package querqy.lucene.rewrite;
 
 import java.io.IOException;
 
-import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
 
 
@@ -20,9 +18,7 @@ public class NullQueryFactory implements LuceneQueryFactory<Query> {
     @Override
     public Query createQuery(DocumentFrequencyCorrection dfc, boolean isBelowDMQ)
             throws IOException {
-        BooleanQuery bq = new BooleanQuery(true);
-        bq.add(new MatchAllDocsQuery(), Occur.MUST_NOT);
-        return bq;
+        return new BooleanQuery();
     }
 
     @Override
