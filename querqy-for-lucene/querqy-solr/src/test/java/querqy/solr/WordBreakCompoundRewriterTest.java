f8e17ac15dcfbe0f54776fba4beb72906828e0d0
Matthias Krueger <code@mkr.io>
2019-01-21 10:10:15 +0100

    Small test for decompounder config

--- querqy-for-lucene/querqy-solr/src/test/java/querqy/solr/WordBreakCompoundRewriterTest.java
+++ querqy-for-lucene/querqy-solr/src/test/java/querqy/solr/WordBreakCompoundRewriterTest.java
@@ -16,14 +16,16 @@ public class WordBreakCompoundRewriterTest extends SolrTestCaseJ4 {
 
     private static void addDocs() {
         assertU(adoc("id", "1",
-                "f1", "herrenjacke"));
-        assertU(adoc("id", "2",
                 "f1", "herren"));
+        assertU(adoc("id", "2",
+                "f1", "herren schöne jacke"));
         assertU(adoc("id", "3",
                 "f1", "damen"));
-        assertU(adoc("id", "3",
+        assertU(adoc("id", "4",
                 "f1", "jacke",
                 "f2", "kinder"));
+        assertU(adoc("id", "5",
+                "f1", "damenjacke lila"));
         assertU(commit());
     }
 
@@ -32,14 +34,73 @@ public class WordBreakCompoundRewriterTest extends SolrTestCaseJ4 {
         String q = "schöne herrenjacke";
 
         SolrQueryRequest req = req("q", q,
-                DisMaxParams.QF, "f1 f2 f3",
+                DisMaxParams.QF, "f1 f2",
+                DisMaxParams.MM, "100%",
                 "defType", "querqy",
                 "debugQuery", "on"
         );
 
-        // todo: test for existence of (herrenjacke OR herren) (herrenjacke OR jacke)
+        assertQ("Decompounding failed",
+                req,
+                "//result[@name='response' and @numFound='1']/doc[1]/str[@name='id'][text()='2']"
+        );
 
-        // ...
+        req.close();
+    }
+
+    @Test
+    public void testWordbreakDecompoundingValidatesAgainstConfiguredField() {
+        String q = "kinderjacke";
+
+        SolrQueryRequest req = req("q", q,
+                DisMaxParams.QF, "f1 f2",
+                DisMaxParams.MM, "100%",
+                "defType", "querqy",
+                "debugQuery", "on"
+        );
+
+        assertQ("Decompounding found unverified term",
+                req,
+                "//result[@name='response' and @numFound='0']"
+        );
+
+        req.close();
+    }
+
+    @Test
+    public void testCompounding() {
+        String q = "lila damen jacke";
+
+        SolrQueryRequest req = req("q", q,
+                DisMaxParams.QF, "f1 f2",
+                DisMaxParams.MM, "100%",
+                "defType", "querqy",
+                "debugQuery", "on"
+        );
+
+        assertQ("Compounding failed",
+                req,
+                "//result[@name='response' and @numFound='1']/doc[1]/str[@name='id'][text()='5']"
+        );
+
+        req.close();
+    }
+
+    @Test
+    public void testCompoundingTriggerReverseCompound() {
+        String q = "jacke für damen lila";
+
+        SolrQueryRequest req = req("q", q,
+                DisMaxParams.QF, "f1 f2",
+                DisMaxParams.MM, "100%",
+                "defType", "querqy",
+                "debugQuery", "on"
+        );
+
+        assertQ("Compounding with reverse trigger failed",
+                req,
+                "//result[@name='response' and @numFound='1']/doc[1]/str[@name='id'][text()='5']"
+        );
 
         req.close();
     }
