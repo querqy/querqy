4afe5cfd95602653e516c4e82c874cc50f66416b
René Kriegler <post@rene-kriegler.com>
2019-08-05 17:56:13 +0100

    TemScorer.getMaxScore no longer consumes PostingsEnum. Fixes #60

--- querqy-for-lucene/querqy-lucene/src/main/java/querqy/lucene/rewrite/TermScorer.java
+++ querqy-for-lucene/querqy-lucene/src/main/java/querqy/lucene/rewrite/TermScorer.java
@@ -5,73 +5,83 @@ package querqy.lucene.rewrite;
  */
 import java.io.IOException;
 
+import org.apache.lucene.index.ImpactsEnum;
 import org.apache.lucene.index.PostingsEnum;
+import org.apache.lucene.index.SlowImpactsEnum;
 import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.ImpactsDISI;
 import org.apache.lucene.search.LeafSimScorer;
 import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.Weight;
-import org.apache.lucene.search.similarities.Similarity;
 
 /** Expert: A <code>Scorer</code> for documents matching a <code>Term</code>.
  */
 final class TermScorer extends Scorer {
-  private final PostingsEnum postingsEnum;
-  private final LeafSimScorer docScorer;
+    private final PostingsEnum postingsEnum;
+    private final ImpactsEnum impactsEnum;
+    private final DocIdSetIterator iterator;
+    private final LeafSimScorer docScorer;
+    private final ImpactsDISI impactsDisi;
 
-  /**
-   * Construct a <code>TermScorer</code>.
-   *
-   * @param weight
-   *          The weight of the <code>Term</code> in the query.
-   * @param td
-   *          An iterator over the documents matching the <code>Term</code>.
-   * @param docScorer
-   *          The </code>Similarity.SimScorer</code> implementation
-   *          to be used for score computations.
-   */
-  TermScorer(final Weight weight, final PostingsEnum td, final LeafSimScorer docScorer) {
-    super(weight);
-    this.docScorer = docScorer;
-    this.postingsEnum = td;
-  }
-
-  @Override
-  public int docID() {
-    return postingsEnum.docID();
-  }
-
-
-  public int freq() throws IOException {
-    return postingsEnum.freq();
-  }
-
-  @Override
-  public DocIdSetIterator iterator() { return postingsEnum; }
-
-  @Override
-  public float getMaxScore(int upTo) throws IOException {
-
-    float maxScore = Float.MIN_VALUE;
-    int nextDoc = postingsEnum.nextDoc();
-    while (nextDoc < upTo) {
-      final float score = docScorer.score(nextDoc, postingsEnum.freq());
-      maxScore = Math.max(maxScore, score);
-      nextDoc = postingsEnum.nextDoc();
+    /**
+     * Construct a {@link org.apache.lucene.search.TermScorer} that will iterate all documents.
+     */
+    TermScorer(Weight weight, PostingsEnum postingsEnum, LeafSimScorer docScorer) {
+        super(weight);
+        iterator = this.postingsEnum = postingsEnum;
+        impactsEnum = new SlowImpactsEnum(postingsEnum);
+        impactsDisi = new ImpactsDISI(impactsEnum, impactsEnum, docScorer.getSimScorer());
+        this.docScorer = docScorer;
     }
-    return maxScore;
-  }
+
+    /**
+     * Construct a {@link org.apache.lucene.search.TermScorer} that will use impacts to skip blocks of
+     * non-competitive documents.
+     */
+    TermScorer(Weight weight, ImpactsEnum impactsEnum, LeafSimScorer docScorer) {
+        super(weight);
+        postingsEnum = this.impactsEnum = impactsEnum;
+        impactsDisi = new ImpactsDISI(impactsEnum, impactsEnum, docScorer.getSimScorer());
+        iterator = impactsDisi;
+        this.docScorer = docScorer;
+    }
+
+    @Override
+    public int docID() {
+        return postingsEnum.docID();
+    }
+
+    final int freq() throws IOException {
+        return postingsEnum.freq();
+    }
+
+    @Override
+    public DocIdSetIterator iterator() {
+        return iterator;
+    }
 
     @Override
     public float score() throws IOException {
-        assert docID() != DocIdSetIterator.NO_MORE_DOCS; // FIXME  - Wrong arguments
+        assert docID() != DocIdSetIterator.NO_MORE_DOCS;
         return docScorer.score(postingsEnum.docID(), postingsEnum.freq());
     }
 
-    public LeafSimScorer getDocScorer() {
-        return docScorer;
+    @Override
+    public int advanceShallow(int target) throws IOException {
+        return impactsDisi.advanceShallow(target);
+    }
+
+    @Override
+    public float getMaxScore(int upTo) throws IOException {
+        return impactsDisi.getMaxScore(upTo);
+    }
+
+    @Override
+    public void setMinCompetitiveScore(float minScore) {
+        impactsDisi.setMinCompetitiveScore(minScore);
     }
 
     /** Returns a string representation of this <code>TermScorer</code>. */
-  @Override
-  public String toString() { return "scorer(" + weight + ")[" + super.toString() + "]"; }
+    @Override
+    public String toString() { return "scorer(" + weight + ")[" + super.toString() + "]"; }
 }
