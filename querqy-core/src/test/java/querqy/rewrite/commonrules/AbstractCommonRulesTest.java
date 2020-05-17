edf96b8d024ce1c14ec68f13471e87fe052a5817
JohannesDaniel <johannesdaniel.peter@gmx.de>
2020-05-17 22:08:18 +0200

    tests to explore behavior of DeleteInstruction and SynonymInstruction interactions

--- querqy-core/src/test/java/querqy/rewrite/commonrules/AbstractCommonRulesTest.java
+++ querqy-core/src/test/java/querqy/rewrite/commonrules/AbstractCommonRulesTest.java
@@ -3,21 +3,32 @@ package querqy.rewrite.commonrules;
 import static querqy.rewrite.commonrules.select.SelectionStrategyFactory.DEFAULT_SELECTION_STRATEGY;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.junit.Before;
 import querqy.model.ExpandedQuery;
 import querqy.model.InputSequenceElement;
 import querqy.parser.WhiteSpaceQuerqyParser;
 import querqy.rewrite.commonrules.model.Action;
+import querqy.rewrite.commonrules.model.DeleteInstruction;
+import querqy.rewrite.commonrules.model.Input;
+import querqy.rewrite.commonrules.model.Instruction;
+import querqy.rewrite.commonrules.model.Instructions;
 import querqy.rewrite.commonrules.model.PositionSequence;
 import querqy.rewrite.commonrules.model.RulesCollection;
+import querqy.rewrite.commonrules.model.RulesCollectionBuilder;
+import querqy.rewrite.commonrules.model.SynonymInstruction;
 import querqy.rewrite.commonrules.model.Term;
 import querqy.rewrite.commonrules.select.TopRewritingActionCollector;
 
 public abstract class AbstractCommonRulesTest {
-    
+
+    private int ruleCounter = 0;
+
     public final static Map<String, Object> EMPTY_CONTEXT = new HashMap<>();
 
     protected ExpandedQuery makeQuery(String input) {
@@ -37,5 +48,27 @@ public abstract class AbstractCommonRulesTest {
         rules.collectRewriteActions(seq, collector);
         return collector.createActions();
     }
+
+    public DeleteInstruction delete(String... terms) {
+        return new DeleteInstruction(Arrays.stream(terms).map(this::mkTerm).collect(Collectors.toList()));
+    }
+
+    public SynonymInstruction synonym(String... terms) {
+        return new SynonymInstruction(Arrays.stream(terms).map(this::mkTerm).collect(Collectors.toList()));
+    }
+
+    public Input input(String... terms) {
+        return new Input(Arrays.stream(terms).map(this::mkTerm).collect(Collectors.toList()), false, false, "");
+    }
+
+    public void addRule(RulesCollectionBuilder builder, Input input, Instruction... instructions) {
+        int ruleCount = ruleCounter++;
+        builder.addRule(input, new Instructions(ruleCount, ruleCount, Arrays.asList(instructions)));
+    }
+
+    public static List<String> list(String... items) {
+        return Arrays.asList(items);
+    }
+
 
 }
