8c1f664efa44d772154ce69a524954c7bf7a51d3
JohannesDaniel <johannesdaniel.peter@gmx.de>
2020-06-01 22:20:06 +0200

    Enhanced LookupUtils by additional Comparators

--- querqy-core/src/main/java/querqy/trie/LookupUtils.java
+++ querqy-core/src/main/java/querqy/trie/LookupUtils.java
@@ -1,6 +1,8 @@
 package querqy.trie;
 
 import querqy.trie.model.ExactMatch;
+import querqy.trie.model.PrefixMatch;
+import querqy.trie.model.SuffixMatch;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -13,17 +15,28 @@ public class LookupUtils {
 
     private LookupUtils(){}
 
-    private static final Comparator<ExactMatch<?>> COMPARE_BY_SIZE_DESC = (o1, o2) -> o2.termSize - o1.termSize;
-    private static final Comparator<ExactMatch<?>> COMPARE_BY_LOOKUP_START_ASC = Comparator.comparingInt(o -> o.lookupStart);
+    private static final Comparator<ExactMatch<?>> COMPARE_EXACT_MATCH_BY_SIZE_DESC = (o1, o2) -> o2.termSize - o1.termSize;
+
+    private static final Comparator<ExactMatch<?>> COMPARE_EXACT_MATCH_BY_LOOKUP_START_ASC =
+            Comparator.comparingInt(o -> o.lookupStart);
+
+    public static final Comparator<ExactMatch<?>> COMPARE_EXACT_MATCH_BY_LOOKUP_OFFSET_DESC =
+            (o1, o2) -> o2.lookupStart - o1.lookupStart;
 
     public static final Comparator<State<?>> COMPARE_STATE_BY_INDEX_DESC = Comparator.comparingInt(o -> o.index);
+
+    public static final Comparator<SuffixMatch<?>> COMPARE_SUFFIX_MATCH_BY_LOOKUP_OFFSET_DESC =
+            (o1, o2) -> o2.getLookupOffset() - o1.getLookupOffset();
+
+    public static final Comparator<PrefixMatch<?>> COMPARE_PREFIX_MATCH_BY_LOOKUP_OFFSET_DESC =
+            (o1, o2) -> o2.getLookupOffset() - o1.getLookupOffset();
 
     public static <T> List<ExactMatch<T>> removeSubsetsAndSmallerOverlaps(List<ExactMatch<T>> exactMatches) {
 
         final List<ExactMatch<T>> exactMatchesFiltered = new ArrayList<>();
 
         final List<ExactMatch<T>> exactMatchesSorted = new ArrayList<>(exactMatches);
-        exactMatchesSorted.sort(COMPARE_BY_SIZE_DESC.thenComparing(COMPARE_BY_LOOKUP_START_ASC));
+        exactMatchesSorted.sort(COMPARE_EXACT_MATCH_BY_SIZE_DESC.thenComparing(COMPARE_EXACT_MATCH_BY_LOOKUP_START_ASC));
 
         exactMatchesSorted.stream()
                 .filter(current -> exactMatchesFiltered.stream()
@@ -31,7 +44,7 @@ public class LookupUtils {
                                 compared.lookupStart, compared.lookupExclusiveEnd - 1)))
                 .forEach(exactMatchesFiltered::add);
 
-        exactMatchesFiltered.sort(COMPARE_BY_LOOKUP_START_ASC);
+        exactMatchesFiltered.sort(COMPARE_EXACT_MATCH_BY_LOOKUP_START_ASC);
 
         return exactMatchesFiltered;
     }
--- querqy-core/src/test/java/querqy/trie/LookupUtilsTest.java
+++ querqy-core/src/test/java/querqy/trie/LookupUtilsTest.java
@@ -2,6 +2,8 @@ package querqy.trie;
 
 import org.junit.Test;
 import querqy.trie.model.ExactMatch;
+import querqy.trie.model.PrefixMatch;
+import querqy.trie.model.SuffixMatch;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,6 +12,63 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LookupUtilsTest {
+
+    @Test
+    public void testSortingOfSuffixMatch() {
+        List<SuffixMatch<String>> suffixMatches = Arrays.asList(
+                suffixMatch(0),
+                suffixMatch(4),
+                suffixMatch(3),
+                suffixMatch(1)
+        );
+
+        suffixMatches.sort(LookupUtils.COMPARE_SUFFIX_MATCH_BY_LOOKUP_OFFSET_DESC);
+
+        assertThat(suffixMatches).containsExactly(
+                suffixMatch(4),
+                suffixMatch(3),
+                suffixMatch(1),
+                suffixMatch(0)
+        );
+    }
+
+    @Test
+    public void testSortingOfPrefixMatch() {
+        List<PrefixMatch<String>> prefixMatches = Arrays.asList(
+                prefixMatch(0),
+                prefixMatch(4),
+                prefixMatch(3),
+                prefixMatch(1)
+        );
+
+        prefixMatches.sort(LookupUtils.COMPARE_PREFIX_MATCH_BY_LOOKUP_OFFSET_DESC);
+
+        assertThat(prefixMatches).containsExactly(
+                prefixMatch(4),
+                prefixMatch(3),
+                prefixMatch(1),
+                prefixMatch(0)
+        );
+    }
+
+    @Test
+    public void testSortingOfExactMatchByLookupOffset() {
+        List<ExactMatch<String>> exactMatches = Arrays.asList(
+                exactMatch(1, 2),
+                exactMatch(0, 1),
+                exactMatch(4, 5),
+                exactMatch(2, 3)
+        );
+
+        exactMatches.sort(LookupUtils.COMPARE_EXACT_MATCH_BY_LOOKUP_OFFSET_DESC);
+
+        assertThat(exactMatches).containsExactly(
+                exactMatch(4, 5),
+                exactMatch(2, 3),
+                exactMatch(1, 2),
+                exactMatch(0, 1)
+        );
+    }
 
     @Test
     public void testManyOverlappingBoundaries() {
@@ -96,5 +155,13 @@ public class LookupUtilsTest {
 
     private ExactMatch<String> exactMatch(int lookupStart, int lookupExclusiveEnd) {
         return new ExactMatch<>(lookupStart, lookupExclusiveEnd, "");
+    }
+
+    private PrefixMatch<String> prefixMatch(int lookupOffset) {
+        return new PrefixMatch<>(0, "", "").setLookupOffset(lookupOffset);
+    }
+
+    private SuffixMatch<String> suffixMatch(int lookupOffset) {
+        return new SuffixMatch<>(0, "", "").setLookupOffset(lookupOffset);
     }
 }
