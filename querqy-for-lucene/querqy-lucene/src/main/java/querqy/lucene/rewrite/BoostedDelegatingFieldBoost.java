14d046a8ec9b15cc67d0a1d2372ee79c372ef203
Torsten Bøgh Köster <tbk@thiswayup.de>
2021-01-18 11:59:01 +0100

    [#128] add proper toString() and equals() // hashCode()

--- querqy-for-lucene/querqy-lucene/src/main/java/querqy/lucene/rewrite/BoostedDelegatingFieldBoost.java
+++ querqy-for-lucene/querqy-lucene/src/main/java/querqy/lucene/rewrite/BoostedDelegatingFieldBoost.java
@@ -1,6 +1,7 @@
 package querqy.lucene.rewrite;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import org.apache.lucene.index.IndexReader;
 
@@ -29,8 +30,28 @@ public class BoostedDelegatingFieldBoost implements FieldBoost {
 
     @Override
     public String toString(final String fieldname) {
-        return delegate.toString(fieldname);
+        return "^BoostedDelegatingFieldBoost(" 
+            + delegate.toString(fieldname) 
+            + "^" + boost + ")";
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BoostedDelegatingFieldBoost other = (BoostedDelegatingFieldBoost) obj;
+        if (!delegate.equals(other.delegate))
+            return false;
+        return Float.compare(boost, other.boost) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(delegate.hashCode(), boost);
+    }
 
 }
