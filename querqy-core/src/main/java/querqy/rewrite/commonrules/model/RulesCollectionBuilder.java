c0c7ddb3d5a3c857a525510f1b8ae94670195bc6
JohannesDaniel <johannesdaniel.peter@gmx.de>
2021-09-21 17:27:14 +0200

    Enhanced RulesCollectionBuilder also to accept Rule

--- querqy-core/src/main/java/querqy/rewrite/commonrules/model/RulesCollectionBuilder.java
+++ querqy-core/src/main/java/querqy/rewrite/commonrules/model/RulesCollectionBuilder.java
@@ -2,12 +2,15 @@ package querqy.rewrite.commonrules.model;
 
 import querqy.model.Input;
 import querqy.rewrite.commonrules.select.booleaninput.model.BooleanInputLiteral;
+import querqy.rewrite.rules.rule.Rule;
 
 public interface RulesCollectionBuilder {
 
     void addRule(Input.SimpleInput input, Instructions instructions);
 
     void addRule(Input.SimpleInput input, BooleanInputLiteral literal);
+
+    void addRule(final Rule rule);
 
     RulesCollection build();
 
