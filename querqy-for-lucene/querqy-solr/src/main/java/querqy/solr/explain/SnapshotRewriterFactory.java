ca067cd7c245b08e545beff96d78bd25528dab8f
René Kriegler <post@rene-kriegler.com>
2021-10-06 09:20:06 +0200

    WIP: Explain rewrite chain #187

---
+++ querqy-for-lucene/querqy-solr/src/main/java/querqy/solr/explain/SnapshotRewriterFactory.java
@@ -0,0 +1,40 @@
+package querqy.solr.explain;
+
+import querqy.model.ExpandedQuery;
+import querqy.rewrite.QueryRewriter;
+import querqy.rewrite.RewriterFactory;
+import querqy.rewrite.SearchEngineRequestAdapter;
+
+import java.util.Map;
+
+public class SnapshotRewriterFactory extends RewriterFactory {
+
+    final static String NAME = "__EXPLAIN_SNAPSHOT";
+
+    private SnapshotRewriter rewriter = null;
+
+    private final String previousRewriterId;
+
+    public SnapshotRewriterFactory(final String previousRewriterId) {
+        super(NAME + "/" + previousRewriterId);
+        this.previousRewriterId = previousRewriterId;
+    }
+
+    @Override
+    public synchronized QueryRewriter createRewriter(final ExpandedQuery input,
+                                                     final SearchEngineRequestAdapter searchEngineRequestAdapter) {
+        if (rewriter != null) {
+            throw new IllegalStateException("This factory can only be used once!");
+        }
+        this.rewriter = new SnapshotRewriter();
+        return rewriter;
+    }
+
+    public Map<String, Object> getSnapshot() {
+        return rewriter.getSnapshot();
+    }
+
+    public String getPreviousRewriterId() {
+        return previousRewriterId;
+    }
+}
