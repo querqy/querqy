4a1200d6a6e084ae4e4d93ddc7eaa4f92429fff4
= <danworley@gmail.com>
2021-11-03 12:29:35 -0400

    Issue #187: Build out handler tests

--- querqy-for-lucene/querqy-solr/src/test/java/querqy/solr/explain/ExplainRewriteChainTest.java
+++ querqy-for-lucene/querqy-solr/src/test/java/querqy/solr/explain/ExplainRewriteChainTest.java
@@ -1,15 +1,12 @@
 package querqy.solr.explain;
 
-
 import static querqy.solr.QuerqyQParserPlugin.PARAM_REWRITERS;
 import static querqy.solr.StandaloneSolrTestSupport.withCommonRulesRewriter;
 
 import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.request.SolrQueryRequest;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-import querqy.solr.QuerqyQParserPlugin;
 import querqy.solr.rewriter.commonrules.CommonRulesConfigRequestBuilder;
 
 @SolrTestCaseJ4.SuppressSSL
@@ -18,16 +15,13 @@ public class ExplainRewriteChainTest  extends SolrTestCaseJ4 {
     @BeforeClass
     public static void beforeTests() throws Exception {
         initCore("solrconfig.xml", "schema.xml");
-
-
     }
 
     @Test
     public void testExplainChain() {
-        // FIXME: complete test
         final String rewriterName = "conf_common_rules";
         final CommonRulesConfigRequestBuilder builder = new CommonRulesConfigRequestBuilder()
-                .rules("a =>\n SYNONYM: b\nFILTER: x\nUP(100): pears").ignoreCase(true);
+                .rules("a =>\n SYNONYM: b\nSYNONYM(2.5): f1:c\nFILTER: x\nUP(100): pears").ignoreCase(true);
         withCommonRulesRewriter(h.getCore(), rewriterName, builder);
 
 
@@ -40,7 +34,18 @@ public class ExplainRewriteChainTest  extends SolrTestCaseJ4 {
 
             assertQ("Error explaining chain",
                     req,
-                    "//lst[@name='explain']/str[@name='query_string'][text()='a']"
+                    "//lst[@name='explain']/str[@name='query_string'][text()='a']",
+
+                    "//lst[@name='MATCHING_QUERY'][1]//arr[@name='clauses']//lst[1]//bool[@name='generated'][text()='true']",
+                    "//lst[@name='MATCHING_QUERY'][1]//arr[@name='clauses']//lst[1]//str[@name='field'][text()='f1']",
+                    "//lst[@name='MATCHING_QUERY'][1]//arr[@name='clauses']//lst[1]//float[@name='boost'][text()='2.5']",
+
+                    "//lst[@name='BOOST_QUERIES']//bool[@name='generated'][text()='true']",
+                    "//lst[@name='BOOST_QUERIES']//str[@name='value'][text()='pears']",
+                    "//lst[@name='BOOST_QUERIES']//float[@name='factor'][text()='100.0']",
+
+                    "//arr[@name='FILTER_QUERIES']//str[@name='value'][text()='x']",
+                    "//arr[@name='FILTER_QUERIES']//bool[@name='generated'][text()='true']"
             );
 
         }
