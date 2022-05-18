9947a98294195e462f21d889ccbbc1466b8e2370
Kevin Risden <krisden@apache.org>
2022-05-18 14:56:30 -0400

    Stopwords testing

--- querqy-for-lucene/querqy-solr/src/test/java/querqy/solr/QuerqyDismaxQParserPluginWithStopwordsTest.java
+++ querqy-for-lucene/querqy-solr/src/test/java/querqy/solr/QuerqyDismaxQParserPluginWithStopwordsTest.java
@@ -4,6 +4,7 @@ import static querqy.solr.QuerqyQParserPlugin.PARAM_REWRITERS;
 import static querqy.solr.StandaloneSolrTestSupport.withCommonRulesRewriter;
 
 import org.apache.solr.SolrTestCaseJ4;
+import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.DisMaxParams;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.QueryParsing;
@@ -15,18 +16,13 @@ import org.junit.Test;
 public class QuerqyDismaxQParserPluginWithStopwordsTest extends SolrTestCaseJ4 {
 
     public void index() {
-
-        assertU(adoc("id", "1", "f1_stopwords", "a", "f2_stopwords", "c"));
-
-        assertU(adoc("id", "2", "f1_stopwords", "a", "f2_stopwords", "b"));
-        
-        assertU(adoc("id", "3", "f1_stopwords", "a", "f3", "c"));
-        
-        assertU(adoc("id", "4", "f3", "stopA"));
+        assertU(adoc("id", "1", "f1", "a"));
+        assertU(adoc("id", "2", "f1", "a"));
+        assertU(adoc("id", "3", "f1", "a"));
+        assertU(adoc("id", "4", "f1", "stopA"));
 
         assertU(commit());
-
-     }
+    }
 
     @BeforeClass
     public static void beforeTests() throws Exception {
@@ -41,25 +37,148 @@ public class QuerqyDismaxQParserPluginWithStopwordsTest extends SolrTestCaseJ4 {
         clearIndex();
         index();
     }
-     
+
     @Test
-    public void testThatStopwordOnlyQueryReturnsNoResult() {
-
-        String q = "stopA";
-
+    public void testQueryWithStopwords() {
+        // Simple test make sure data loaded
+        String q = "a";
         SolrQueryRequest req = req("q", q,
-                DisMaxParams.QF, "f1_stopwords f2_stopwords",
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.TIE, "0.1",
+                "defType", "edismax",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("Expected 3 results",
+                req,
+                "//result[@name='response'][@numFound='3']");
+
+        req.close();
+
+        q = "a";
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
                 QueryParsing.OP, "OR",
                 DisMaxParams.TIE, "0.1",
                 "defType", "querqy",
                 PARAM_REWRITERS, "common_rules_empty",
-                "debugQuery", "true"
-              );
-        assertQ("No results expected",
-              req,
-              "//result[@name='response'][@numFound='0']");
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("Expected 3 results",
+                req,
+                "//result[@name='response'][@numFound='3']");
 
         req.close();
-     }
+
+
+        // Check that no documents are found for only stop word
+        q = "stopA";
+
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.TIE, "0.1",
+                "defType", "edismax",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("No results expected",
+                req,
+                "//result[@name='response'][@numFound='0']");
+
+        req.close();
+
+        q = "stopA";
+
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.TIE, "0.1",
+                "defType", "querqy",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("No results expected",
+                req,
+                "//result[@name='response'][@numFound='0']");
+
+        req.close();
+
+        // Test mix stop words and regular work without mm
+        q = "stopA a";
+
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.TIE, "0.1",
+                "defType", "edismax",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("Expected 3 results",
+                req,
+                "//result[@name='response'][@numFound='3']");
+
+        req.close();
+
+        q = "stopA a";
+
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.TIE, "0.1",
+                "defType", "querqy",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("Expected 3 results",
+                req,
+                "//result[@name='response'][@numFound='3']");
+
+        req.close();
+
+        // Test mix stop words and regular work with mm
+
+        q = "stopA a";
+
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.MM, "2",
+                "defType", "edismax",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("Expected 3 results",
+                req,
+                "//result[@name='response'][@numFound='3']");
+
+        req.close();
+
+        q = "stopA a";
+
+        req = req("q", q,
+                DisMaxParams.QF, "id f1",
+                QueryParsing.OP, "OR",
+                DisMaxParams.MM, "2",
+                "defType", "querqy",
+                PARAM_REWRITERS, "common_rules_empty",
+                CommonParams.DEBUG, "true",
+                "indent", "true"
+        );
+        assertQ("Expected 3 results",
+                req,
+                "//result[@name='response'][@numFound='3']");
+
+        req.close();
+    }
 
 }
